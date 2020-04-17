31c824ff6f963c2d106597b56eff15546409c5ec
Javed Memon <javedmemon1920@gmail.com>
2020-04-18 04:38:24 +0500

    Created new screen

---
+++ legal_consutancy/lib/main_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainScreenOfApp extends StatefulWidget {
+  @override
+  _MainScreenOfAppState createState() => _MainScreenOfAppState();
+}
+
+class _MainScreenOfAppState extends State<MainScreenOfApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold()
+    );
+  }
+}
