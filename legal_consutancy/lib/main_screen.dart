25a4f11f9de1074333dc74388054082be484c81b
Javed Memon <javedmemon1920@gmail.com>
2020-04-22 20:10:50 +0500

    did some changes in category screen

--- legal_consutancy/lib/main_screen.dart
+++ legal_consutancy/lib/main_screen.dart
@@ -9,36 +9,47 @@ class MainScreenOfApp extends StatefulWidget {
 class _MainScreenOfAppState extends State<MainScreenOfApp> {
   @override
   Widget build(BuildContext context) {
-
-    Widget catagoriesListItem(String str){
-      return ListTile(
-        leading: Text(str),
-        trailing: Icon(Icons.arrow_right),
-        dense: true,
-        enabled: true,
-        selected: true,
-        isThreeLine: true,
-
+    Widget catagoriesListItem(String str) {
+      return Container(
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          //color: Color.fromRGBO(00, 69, 69, 1),
+          color: Color.fromRGBO(0,105,105,1),
+          borderRadius: BorderRadius.circular(25.0)
+        ),
+        margin: EdgeInsets.all(4.0),
+        child: ListTile(
+          leading: Text(
+            str,
+            style: TextStyle(color: Colors.white),
+          ),
+          trailing: Icon(
+            Icons.arrow_right,
+            color: Colors.white,
+          ),
+        ),
       );
     }
 
     return Container(
-      child: Scaffold(
-        appBar: AppBar(title: Text('Categories'),),
-        body: Column(
-          children: <Widget>[
-            catagoriesListItem('Banking'),
-            catagoriesListItem('Business'),
-            catagoriesListItem('Civil'),
-            catagoriesListItem('Criminal'),
-            catagoriesListItem('Cyber Crime'),
-            catagoriesListItem('Divorse'),
-            catagoriesListItem('Finance'),
-            catagoriesListItem('Tax'),
-            catagoriesListItem('Other')
-          ],
-        ),
-      )
-    );
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text('Categories'),
+        backgroundColor: Color.fromRGBO(00, 69, 69, 1),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: catagoriesListItem('Banking')),
+          Expanded(child: catagoriesListItem('Business')),
+          Expanded(child: catagoriesListItem('Civil')),
+          Expanded(child: catagoriesListItem('Criminal')),
+          Expanded(child: catagoriesListItem('Cyber Crime')),
+          Expanded(child: catagoriesListItem('Divorse')),
+          Expanded(child: catagoriesListItem('Finance')),
+          Expanded(child: catagoriesListItem('Tax')),
+          Expanded(child: catagoriesListItem('Other'))
+        ],
+      ),
+    ));
   }
 }
