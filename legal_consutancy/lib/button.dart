78bd834a3b7d343b525b99eb43c614a84151945c
Javed Memon <javedmemon1920@gmail.com>
2020-11-02 00:07:05 +0500

    seperate files for text field and buttons

---
+++ legal_consutancy/lib/button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Widget loginScreenButton(String str) {
+    return RaisedButton(
+      onPressed: (){},
+      textColor: Colors.black,
+      color: Colors.white,
+      padding: const EdgeInsets.all(0.0),
+      child: Container(
+        alignment: Alignment.center,
+        child: Text(str),
+        width: 200.0,
+        height: 50.0,
+      ),
+    );
+  }
